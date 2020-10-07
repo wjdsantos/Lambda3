a58835964624d911b83626e78c28e35a58662356
Walter dos Santos <walter4asantos@gmail.com>
2020-10-07 16:07:12 -0300

    Expressao lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,6 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -11,7 +10,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-			//Expressão lambda declarada
+			//Expressão lambda inline
 		Locale.setDefault(Locale.US);
 		
 		List<Product> list = new ArrayList<>();
@@ -22,13 +21,9 @@ public class Program {
 		list.add(new Product("HD Case", 80.90));
 
 		double factor = 1.1;
-		Consumer<Product> cons = p -> {		//Criando variavel do tipo Consumer para fazer a atualização dos preços em 10% 
-			p.setPrice(p.getPrice() * factor); //As chaves foram colocadas somente para mostrar que podem ser usadas também
-		};									//Mas o normal seria não utiliza-las e levar o calculo para a mesma linha
-											//da declaração da variável.
 		
-		list.forEach(cons);  //Implementação de Consumer em declaração direta no programa, usando a variavel.
-								//Não usando a referencia do metodo da classe Product
+		list.forEach(p -> p.setPrice(p.getPrice() * factor)); //Pegamos a expressao lambda e colocamos direto no argumento 
+															// da função. Sem as chaves...
 		
 		list.forEach(System.out::println); //Usando um referente method para o println
 	}
