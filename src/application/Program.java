34b5a2c5f3dddd9ef7f3fe4aca4459b03fc71591
Walter dos Santos <walter4asantos@gmail.com>
2020-10-07 15:24:34 -0300

    Reference method com metodo nao estática

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,13 +5,12 @@ import java.util.List;
 import java.util.Locale;
 
 import entities.Product;
-import util.PriceUpdate;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-			//Reference method com método estático
+			//Reference method com método não estático
 		Locale.setDefault(Locale.US);
 		
 		List<Product> list = new ArrayList<>();
@@ -21,8 +20,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		list.forEach(Product::staticPriceUpdate);  //Implementação de Consumer em uma classe interface com um metodo 
-		                                  // statico que acrescenta 10% nos valores da lista
+		list.forEach(Product::nonStaticPriceUpdate);  //Implementação de Consumer em uma classe interface com um metodo 
+		                                  // não statico, ou seja que trabalha com o proprio atributo.
 		
 		list.forEach(System.out::println); //Usando um referente method para o println
 	}
