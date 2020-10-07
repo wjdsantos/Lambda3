5a0d735ba1917b8ddffab2327b98729b98046371
Walter dos Santos <walter4asantos@gmail.com>
2020-10-07 16:00:33 -0300

    Expressao lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -10,7 +11,7 @@ public class Program {
 
 	public static void main(String[] args) {
 
-			//Reference method com método não estático
+			//Expressão lambda declarada
 		Locale.setDefault(Locale.US);
 		
 		List<Product> list = new ArrayList<>();
@@ -20,8 +21,14 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		list.forEach(Product::nonStaticPriceUpdate);  //Implementação de Consumer em uma classe interface com um metodo 
-		                                  // não statico, ou seja que trabalha com o proprio atributo.
+		double factor = 1.1;
+		Consumer<Product> cons = p -> {		//Criando variavel do tipo Consumer para fazer a atualização dos preços em 10% 
+			p.setPrice(p.getPrice() * factor); //As chaves foram colocadas somente para mostrar que podem ser usadas também
+		};									//Mas o normal seria não utiliza-las e levar o calculo para a mesma linha
+											//da declaração da variável.
+		
+		list.forEach(cons);  //Implementação de Consumer em declaração direta no programa, usando a variavel.
+								//Não usando a referencia do metodo da classe Product
 		
 		list.forEach(System.out::println); //Usando um referente method para o println
 	}
