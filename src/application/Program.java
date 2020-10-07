5bf6d688567fe6456d3347cf8b74342ee3556e05
Walter dos Santos <walter4asantos@gmail.com>
2020-10-07 14:35:43 -0300

    Implementacao da interface

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Product;
+import util.PriceUpdate;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+			//Implementação da interface
+		Locale.setDefault(Locale.US);
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		list.forEach(new PriceUpdate());  //Implementação de Consumer em uma classe interface com uma função que 
+		                                  //acrescenta 10% nos valores da lista
+		
+		list.forEach(System.out::println); //Usando um referente method para o println
+	}
+
+}
